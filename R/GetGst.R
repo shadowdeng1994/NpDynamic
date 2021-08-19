8e87588ac03419cc68a81bab9f185116cb4691be
ShadowDeng <49464233+shadowdeng1994@users.noreply.github.com>
2021-08-19 18:25:25 +0800

    Update GetGst.R
    Add move annotation.

--- R/GetGst.R
+++ R/GetGst.R
@@ -4,28 +4,34 @@
 
 #' @export
 GetGst <- function(){
+  # load in preprocessed tree.
   load("PackagedData.RData")
 
+  # Extract the organs' componence for each node
   var.PopCompOnTree <- list()
   var.PopCompOnTree[["L5"]] <- GetPopComponent(var.TreeAnn.L5)
   var.PopCompOnTree[["L6"]] <- GetPopComponent(var.TreeAnn.L6)
-
+  
+  # Extract the organs' componence for each node after shuffling
   var.PopCompOnTree_Shuffle <- list()
   var.PopCompOnTree_Shuffle[["L5"]] <- GetPopComponent_Shuffle(var.TreeAnn.L5)
   var.PopCompOnTree_Shuffle[["L6"]] <- GetPopComponent_Shuffle(var.TreeAnn.L6)
 
+  # Calculate Gst
   var.Gst <-
     rbind(
       GetGstWithPopComp("L5",var.PopCompOnTree),
       GetGstWithPopComp("L6",var.PopCompOnTree)
     )
 
+  # Calculate Gst after shuffling
   var.Gst_Shuffle <-
     rbind(
       GetGstWithPopComp("L5",var.PopCompOnTree_Shuffle),
       GetGstWithPopComp("L6",var.PopCompOnTree_Shuffle)
     )
-
+  
+  # Calcualte one-to-others Gst
   var.Gst_Organ <-
     rbind(
       GetGstWithPopComp_Organ("L5",var.PopCompOnTree),

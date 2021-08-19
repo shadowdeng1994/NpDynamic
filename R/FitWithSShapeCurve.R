af5a826c55f981159d4cad39fdd991d09c914a8c
ShadowDeng <49464233+shadowdeng1994@users.noreply.github.com>
2021-08-19 15:50:34 +0800

    Update FitWithSShapeCurve.R
    Add more annotation.

--- R/FitWithSShapeCurve.R
+++ R/FitWithSShapeCurve.R
@@ -6,8 +6,10 @@
 FitWithSShapeCurve <- function(){
   message("## 1/4 ## Load in OrganInfor.ann")
   load("PackagedData.RData")
+  # Filter organs with sufficient samples.
   tmp.FilterOrgan <- GetFilterOrgan(var.TipInfo.L5,var.TipInfo.L6)
 
+  # Fitting wiht S-shape curve.
   var.SShapeFitting <-
     rbind(
       FitGrowthCurve("L5",tmp.FilterOrgan),

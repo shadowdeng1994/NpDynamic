5d346d9f65f07c8cbb5811b278bf75a29ce4b8c0
ShadowDeng <49464233+shadowdeng1994@users.noreply.github.com>
2021-08-19 19:13:01 +0800

    Update sub__GetTreeAnn.R
    Add more annotation.

--- R/sub__GetTreeAnn.R
+++ R/sub__GetTreeAnn.R
@@ -2,11 +2,13 @@
 #'
 #'
 
+# load in tree file
 LoadInTree <- function(FFFly){
   system.file("TreeData", paste0(FFFly,".iqtree.nwk"), package = "NpDynamic") %>%
     read.tree
 }
 
+# load in node's information
 LoadInTreeAnn <- function(FFFly){
   system.file("TreeData", paste0(FFFly,".iqtree.tsv"), package = "NpDynamic") %>%
     read_tsv(col_names = F) %>% tbl_df %>%
@@ -14,11 +16,13 @@ LoadInTreeAnn <- function(FFFly){
     select(Tip=X1,Organ=A,TID=X2,Status=X3,Filter=X4)
 }
 
+# get tree height for each internal node
 GetNode2Height <- function(TTTree){
   cbind(TTTree$edge,TTTree %>% nodeHeights) %>% as.data.frame %>% tbl_df %>%
     rename(From=V1,Node=V2,FromHeight=V3,NodeHeight=V4)
 }
 
+# get tree height for each terminal node
 GetNode2Tips <- function(TTTree){
   rbind(
     TTTree$tip.label %>% tbl_df %>% mutate(Node=1:n()) %>% select(Node,Tip=value),
@@ -29,6 +33,7 @@ GetNode2Tips <- function(TTTree){
   ) %>% tbl_df
 }
 
+# get organ component for each internal node
 GetNode2Organ <- function(NNNode2Tip,TTTip){
   NNNode2Tip %>% left_join(TTTip) %>%
     group_by(Node,Organ) %>% summarise(Count=n()) %>%
